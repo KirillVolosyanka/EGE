cd5e6242772dbde4e2fc374f8cea718f18266251
outofmakarov <6360058@mail.ru>
2024-10-08 18:31:12 +0300

    FIRST LESSON

--- Task2/PracticeMain/num!.py
+++ Task2/PracticeMain/num!.py
@@ -1,17 +1,22 @@
 '''
-A -> B = not(A) or B
+A -> B = not(A) or B. OUT OF USE: >=!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  ≡ - ==
  ∨ - or
  ∧ - and
  ¬ - not
 '''
 
+# 15939
 print("x y z w")
 for x in 0,1:
     for y in 0,1:
         for z in 0,1:
             for w in 0,1:
-                f = (x and not(y)) or (y == z) or not(w)
+                f = (z and y) or ((not(x) or z) == (not(y) or w))
                 if f == 0:
                     print(x, y, z, w)
+
+
+
+
 
