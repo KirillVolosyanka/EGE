b257516672328a4256561e481b4acf57f705ac32
outofmakarov <6360058@mail.ru>
2024-11-07 18:19:22 +0300

    last lesson on 7th nov

--- Task15/Division/Practice_Theory.py
+++ Task15/Division/Practice_Theory.py
@@ -9,3 +9,28 @@
     if Ok:
         print(A)
 '''
+
+#27412
+
+'''for A in range (1, 100):
+    ok = True
+    for x in range (1,100):
+        f = (x % A == 0) or (x % 6 != 0) or (x % 9 !=0)
+        if f == 0:
+            ok = False
+            break
+    if ok == True:
+        print(A)
+'''
+
+#
+
+for A in range (1, 100):
+    ok = True
+    for x in range (1, 100):
+        f = (A < 50) and ((x % A == 0) or ((x % 10 != 0) or (x % 12 != 0)))
+        if f == False:
+            ok = False
+    if ok == True:
+        print(A)
+
