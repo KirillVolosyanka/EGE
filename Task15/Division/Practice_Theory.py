810dcb8a3c457f677a022f1ccae84271b4161712
Kirill <lliriki30@mail.ru>
2024-11-07 17:58:04 +0300

    Division_8106

---
+++ Task15/Division/Practice_Theory.py
@@ -0,0 +1,11 @@
+# 8106
+'''for A in range(1, 100):
+    Ok = True
+    for x in range(1, 1000):
+        F = (x % A == 0) or ((x % 6 != 0) or (x % 4 != 0))
+        if F == False:
+            Ok = False
+            break
+    if Ok:
+        print(A)
+'''
