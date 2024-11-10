b50be16d7eb8330263c3970f5228ae84a5ec6ff7
outofmakarov <6360058@mail.ru>
2024-11-10 13:39:53 +0300

    HW division

--- Task15/Division/TASK15_DIVISION_HW_1.py
+++ Task15/Division/TASK15_DIVISION_HW_1.py
@@ -5,45 +5,76 @@
 '''_________________________________________________________________________'''
 
 # Number 2
-'''
-    Code is here
-'''
-# Answer: _ _ _ _
+'''for a in range(1,1000):
+    ok = True
+    for x in range(1,1000):
+        if ((( x %2 != 0) or (x % 3 != 0)) or (x + a >= 100)) == False:
+            ok = False
+            break
+    if ok:
+        print(a)
+        break'''
+# Answer: 94
 
 '''_________________________________________________________________________'''
 
 # Number 3
-'''
-    Code is here
-'''
-# Answer: _ _ _ _
+'''for a in range(1,500):
+    ok = True
+    for x in range(1,500):
+        if (((72 % x != 0) or (120 % x !=0)) or (a - x > 100)) == False:
+            ok = False
+            break
+    if ok:
+        print(a)
+        break'''
+# Answer: 125
 
 '''_________________________________________________________________________'''
 
 # Number 4
-'''
-    Code is here
-'''
-# Answer: _ _ _ _
+'''for a in range(1,500):
+    ok = True
+    for x in range(1,500):
+        if ((a < 50) and ((x % a !=0) or (x % 10 != 10) or (x % 18 != 0))) == False:
+            ok = False
+            break
+    if ok:
+        print(a)'''
+
+# Answer: 49
 
 '''_________________________________________________________________________'''
 
 # Number 5
-'''
-    Code is here
-'''
-# Answer: _ _ _ _
+'''b = list(range(70,91))
+for a in range(1,1000):
+    ok = True
+    for x in range(1,1000):
+        if ((x % a == 0) or (not(x in b) or ( x % 22 != 0))) == False:
+            ok = False
+            break
+
+    if ok:
+        print(a)'''
+
+# Answer: 88
 
 # Number 6
-'''
-    Code is here
-'''
-# Answer: _ _ _ _
+'''for a in range(1,500):
+    ok = True
+    for x in range(1,500):
+        if ((x % a != 0 or ((x % 21) + (x % 35 == 0)))) == False:
+            ok = False
+            break
+    if ok:
+        print(a)
+        break'''
+
+# Answer: 5
 
 '''_________________________________________________________________________'''
 
 # Number 7
-'''
-    Code is here
-'''
+
 # Answer: _ _ _ _
