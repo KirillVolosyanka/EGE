4ecf646d2c3741f67d4fc4382ca9a62bfb4cd3f3
Kirill <lliriki30@mail.ru>
2024-11-25 18:42:14 +0300

    Task 14(1)

--- Task14/Task14_Practice.py
+++ Task14/Task14_Practice.py
@@ -1,5 +1,6 @@
 #7761
-'''x = 4**2020 + 2**2017 - 15
+'''
+x = 4**2020 + 2**2017 - 15
 s = []
 while x != 0:
     s.append(x % 2)
@@ -17,14 +18,25 @@ while x != 0:
 print(s.count(2))'''
 
 #13743
-
-'''x = 7**20 + 7**30 - 7**2
+'''
+x = 7**20 + 7**30 - 7**2
 s = []
-
+words = "ABCDE"
 while x != 0:
-    s.append(x%7)
-    x = x //7
+    if (x % 15 > 9):
+        s.append(words[x%15 // 10])
+    else:
+        s.append(x%15)
+    x = x // 15
+s = s.reverse()
+print(s)'''
 
-print(s.count(6))'''
-
-#
+#47218
+'''
+for x in '0123456789ABCDE':
+    x1 = '123' + str(x) + '5'
+    x2 = '1' + str(x) + '233'
+    res = int(x1, 15) + int(x2, 15)
+    if res % 14 == 0:
+        res = res // 14
+        print(res)'''
