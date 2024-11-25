fab349b9963334b4849b55b094c6023001f3386a
Kirill <lliriki30@mail.ru>
2024-11-25 20:05:00 +0300

    Task 14(Difficult number)

--- Task14/Task14_Practice.py
+++ Task14/Task14_Practice.py
@@ -40,3 +40,30 @@ for x in '0123456789ABCDE':
     if res % 14 == 0:
         res = res // 14
         print(res)'''
+
+def hui(n):
+    n = str(n)
+    cunt = 0
+    s = []
+    for i in range(len(n) - 1, -1, -1):
+        if n[i] =="!" or n[i] =="@" or n[i] =="#":
+            if n[i] == "!":
+                s.append(36)
+            if n[i] == "@":
+                s.append(37)
+            if n[i] == "#":
+                s.append(38)
+        else:
+            s.append(int(n[i], 36))
+
+    for i in range(0,len(s)):
+        cunt += s[i] * 39**i
+    return cunt
+
+for x in "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#":
+    for y in "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#":
+        x1 = "58" + x + "723" + y + '49'
+        res = hui(x1)
+        if res % 38 == 0:
+            if hui(y + x)**0.5 % 1 == 0:
+                print(hui(y + x))
