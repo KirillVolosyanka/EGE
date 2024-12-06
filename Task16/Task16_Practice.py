bb03b63c0d108d55a5bb2e27867b06ddd9516499
Kirill <lliriki30@mail.ru>
2024-12-06 14:32:10 +0300

    Task 16 Practice(1)

--- Task16/Task16_Practice.py
+++ Task16/Task16_Practice.py
@@ -3,27 +3,28 @@
 def F(n):
     if n < 3:
         return 2
-    if n % 2 == 0 and n > 2:
+    elif n % 2 == 0:
         return F(n - 2) + F(n - 1) - n
-    if n % 2 != 0 and n > 2:
+    else:
         return F(n - 1) - F(n - 2) + 2 * n
+
 print(F(32))
 '''
 
 # 47220
-'''
+
 slovar = {1 : 1}
 for i in range(2, 2024):
     slovar[i] = i * slovar[i - 1]
 print(slovar[2023] / slovar[2020])
-'''
 
 # Example
 '''
-slovar = {}
+slovar = {1: 12}
 slovar["Vasya"] = "Shpala"
 slovar.pop("Vasya")
 slovar["Vasya"] = "Tuneyadec"
+slovar["Vasya"] = "Chmo"
 print(slovar.keys())
 '''
 
