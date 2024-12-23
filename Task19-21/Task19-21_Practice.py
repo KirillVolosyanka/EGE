f4cfa510c86912ecd86f84d67f3c12f8a629bc3e
outofmakarov <6360058@mail.ru>
2024-12-23 20:55:43 +0300

    HW Task15 Nums

--- Task19-21/Task19-21_Practice.py
+++ Task19-21/Task19-21_Practice.py
@@ -5,7 +5,7 @@
 3. End - sum of two group = 20 or less
 '''
 
-def f(x, y, h):
+'''def f(x, y, h):
     if h == 3 and x + y <= 20:
         return 1
     elif h == 3 and x + y > 20:
@@ -76,4 +76,81 @@ for x in range(10, 100):
 print("====")
 for x in range(10, 100):
     if f1(x, 10, 1) == 1:
+        print(x)'''
+
+#27416
+'''def func(x,y,h):
+    if h == 3 and x + y >= 77:
+        return 1
+    elif h ==3 and x+ y <= 77:
+        return 0
+    elif h < 3 and x >= 77:
+        return 0
+    else:
+        if h % 2 == 0:
+            return func(x+1,y,h+1) or func(x*2,y,h+1) or func(x,y+1,h+1) or func(x,y*2,h+1)
+        else:
+            return func(x+1,y,h+1) or func(x*2,y,h+1) or func(x,y+1,h+1) or func(x,y*2,h+1)
+
+for x in range(1,70):
+    if func(x,7,1) == True:
+        print(x)
+        break
+
+#ans: 18
+'''
+#n20
+
+'''def funcish(x,y,h):
+    if h == 4 and x+y >=77:
+        return 1
+    elif h == 4 and x+y < 77:
+        return 0
+    elif x+y >= 77 and h < 4:
+        return 0
+    else:
+        if h % 2 != 0:
+            return funcish(x+1,y,h+1) or funcish(x*2,y,h+1) or funcish(x,y+1,h+1) or funcish(x,y*2,h+1)
+        else:
+            return funcish(x+1,y,h+1) and funcish(x*2,y,h+1) and funcish(x,y+1,h+1) and funcish(x,y*2,h+1)
+
+for x in range(1,70):
+    if funcish(x,7,1) == True:
+        print(x)
+
+#ans: 3134
+'''
+#n21
+def func(x,y,h):
+    if h == 3 and x + y >= 77:
+        return 1
+    elif h == 3 and x + y < 77:
+        return 0
+    elif h < 3 and x + y >= 77:
+        return 0
+    else:
+        if h % 2 == 0:
+            return func(x+1,y,h+1) or func(x*2,y,h+1) or func(x,y+1,h+1) or func(x,y*2,h+1)
+        else:
+            return func(x+1,y,h+1) and func(x*2,y,h+1) and func(x,y+1,h+1) and func(x,y*2,h+1)
+
+def funcish(x,y,h):
+    if (h == 5 or h == 3) and x + y >= 77:
+        return 1
+    elif h == 5 and x+ y < 77:
+        return 0
+    elif x + y >= 77 and h <5:
+        return 0
+    else:
+        if h % 2 == 0:
+            return funcish(x+1,y,h+1) or funcish(x*2,y,h+1) or funcish(x,y+1,h+1) or funcish(x,y*2,h+1)
+        else:
+            return funcish(x + 1, y, h + 1) and funcish(x * 2, y, h + 1) and funcish(x, y + 1, h + 1) and funcish(x, y * 2,h + 1)
+
+for x in range(1,70):
+    if funcish(x,7,1) == 1:
+        print(x)
+print("----")
+for x in range(1,70):
+    if func(x,7,1) == 1:
         print(x)
