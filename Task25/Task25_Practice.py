93b45a530117c239ae935fa0bf7c314a3cb09b64
Kirill <lliriki30@mail.ru>
2025-02-03 19:29:07 +0300

    Task25_Delit

--- Task25/Task25_Practice.py
+++ Task25/Task25_Practice.py
@@ -61,9 +61,41 @@ for num in range(0,10**9,2031):
 
 
 #60267
-
+'''
 for num in range (0,10**10,2024):
     num_line = str(num)
 #1?2157*4
     if num_line[0] == "1" and num_line[2:6] == "2157" and num_line[-1] == "4":
-        print(num,num/2024)
+        print(num,num/2024)'''
+
+# Vasya
+# РАЗОБРААААААААААААААТЬ
+# c = 0
+# for num in range(102, 10**12, 103):
+#     print(num)
+#     numString = str(num)
+    # if numString[0] == '1' and numString[2:6] == '2139' and numString[-1] == '4':
+    #     c += 1
+
+# 27422
+'''
+def Delit(num):
+    c = 0
+    l = []
+    for i in range(2, num//2 + 1):
+        if num % i == 0:
+            c += 1
+            l.append(i)
+        if c > 2:
+            l.clear()
+            return l
+    if c == 2:
+        return l
+    else:
+        l.clear()
+        return l
+
+for i in range(174457, 174506):
+    if len(Delit(i)):
+        print(Delit(i))'''
+
